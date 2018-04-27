d8c68654bf839ef11e01c5505b0c33e777952da3
KimSeongGyun <seonggyun7358@gmail.com>
2018-04-27 05:31:45 -0400

    trial

---
+++ snuzero/scripts/control_manual.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# license removed for brevity
+# input : steer-degree, speed:m/s
+
+import rospy
+import time
+import serial
+import math
+import sys, select, termios, tty
+from std_msgs.msg import String
+from snuzero.msg import control
+
+
+def init():
+    
+    pub = rospy.Publisher('control_topic', control, queue_size=10)
+    rospy.init_node('control_sender', anonymous=True)
+    rate = rospy.Rate(20)
+    msg = control()
+    am=1
+    estop=0
+    gear=0
+    speed=0 #m/s
+    steer=0 #degree
+    brake=1
+        
+    
+    while not rospy.is_shutdown():
+            
+        key = getKey()
+        print("key" + key)
+        
+        if key == 'q':
+            break
+        elif key == 'a':
+            steer = steer - 1
+            if steer <= -15 :
+                steer = -15
+        elif key == 'd':
+            steer = steer + 1
+            if steer >= 15 :
+                steer = 15
+        elif key == 'w':
+            speed = speed + 0.1
+            if speed >= 3:
+                speed = 3
+        elif key == 's':
+            speed = speed - 0.1
+            if speed < 0:
+                speed = 0
+        elif key == 'e':
+            if gear == 0:
+                gear = 2
+            elif gear == 2:
+                gear = 0
+         
+        
+            
+        rospy.loginfo(msg)
+        
+        msg.am = am
+        msg.estop = estop
+        msg.gear = gear
+        msg.brake = brake
+        msg.speed = round(speed,3)
+        msg.steer = round(steer,3)
+        msg.encoder = 100
+        
+        pub.publish(msg)
+        rate.sleep()
+        
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+if __name__ == '__main__':
+    try:
+        settings = termios.tcgetattr(sys.stdin)
+        init()
+    except rospy.ROSInterruptException:
+        pass
+    
+
+
